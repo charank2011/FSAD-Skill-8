1f7b3d8f53a2eb055849b7be0310c8d711dbceb3
2400031943 <2400031943@student.edu>
2026-04-18 00:58:32 +0530

    exp 8 pushed

--- main.java
+++ main.java
@@ -1,19 +1,15 @@
 public class Main {
 
-    // Entry point of the application
     public static void main(String[] args) {
-        // Combined Resolve: Feature Update version 1.1 + Bug Fix
         System.out.println("Hello, Git! This is version 1.1.1 (Feature + Bug Fix)");
         greet("Student");
         showFeature();
     }
 
-    // Greet method
     public static void greet(String name) {
         System.out.println("Welcome, " + name + "! Learning Git is fun.");
     }
 
-    // Method added in feature-update branch
     public static void showFeature() {
         System.out.println("New Feature: Branching logic implemented.");
     }
