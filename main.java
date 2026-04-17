24b000b94965a6832ceec08bf31a6914cbf9e0fc
2400031943 <2400031943@student.edu>
2026-04-18 00:58:30 +0530

    feature-update: Add showFeature method

--- main.java
+++ main.java
@@ -2,12 +2,18 @@ public class Main {
 
     // Entry point of the application
     public static void main(String[] args) {
-        System.out.println("Hello, Git! This is version 1.0");
+        System.out.println("Hello, Git! This is version 1.1");
         greet("Student");
+        showFeature();
     }
 
     // Greet method
     public static void greet(String name) {
         System.out.println("Welcome, " + name + "! Learning Git is fun.");
+    }
+
+    // Method added in feature-update branch
+    public static void showFeature() {
+        System.out.println("New Feature: Branching logic implemented.");
     }
 }
