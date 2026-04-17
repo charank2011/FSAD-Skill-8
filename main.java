76abb03f52a28451abc2457b2faedf6a5177aa88
2400031943 <2400031943@student.edu>
2026-04-18 00:58:29 +0530

    Initial commit: Add main.java and notes.txt

---
+++ main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    // Entry point of the application
+    public static void main(String[] args) {
+        System.out.println("Hello, Git! This is version 1.0");
+        greet("Student");
+    }
+
+    // Greet method
+    public static void greet(String name) {
+        System.out.println("Welcome, " + name + "! Learning Git is fun.");
+    }
+}
